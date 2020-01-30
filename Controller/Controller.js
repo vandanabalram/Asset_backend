3c03e876150f7807757c4d1d21908918a5ff14cc
vandanabalram <vbalramostb2@gmail.com>
2020-01-30 17:04:39 +0530

    forgotpassword1

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -89,7 +89,7 @@ exports.userSignin = (req, res, next) => {
   UserData.findOne({ email: email })
     .then(user => {
       if (!user) {
-        const error = new Error('A user with this email could not be found.');
+        const error = new Error('User does not exist.');
         error.statusCode = 401;
         throw error;
       }
@@ -99,7 +99,7 @@ exports.userSignin = (req, res, next) => {
     })
     .then(isEqual => {
       if (!isEqual) {
-        const error = new Error('wrong password.');
+        const error = new Error('Incorrect password.');
         error.statusCode = 401;
         throw error;
       }
